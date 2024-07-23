2d7d42c17d79d0bb5cf594c1f9684c4847dcec84
Erick Otenyo <otenyo.erick@gmail.com>
2024-07-23 23:34:56 +0300

    Check if ingestion_record_ids are returned before attempting to upload to wis2box

--- wis2box_adl/src/wis2box_adl/core/registries.py
+++ wis2box_adl/src/wis2box_adl/core/registries.py
@@ -1,7 +1,10 @@
 from django.core.exceptions import ImproperlyConfigured
+import logging
 
 from .registry import Registry, Instance
 from .wis2box_upload import upload_to_wis2box
+
+logger = logging.getLogger(__name__)
 
 
 class Plugin(Instance):
@@ -16,8 +19,12 @@ class Plugin(Instance):
 
     def run_process(self):
         ingestion_record_ids = self.get_data()
-        for record_id in ingestion_record_ids:
-            upload_to_wis2box(record_id)
+
+        if ingestion_record_ids and len(ingestion_record_ids) > 0:
+            for record_id in ingestion_record_ids:
+                upload_to_wis2box(record_id)
+        else:
+            logger.info("[WIS2BOX_ADL_PLUGIN] No ingestion records returned by plugin get_data")
 
 
 class PluginRegistry(Registry):
