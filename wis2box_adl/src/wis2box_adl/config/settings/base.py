5ece607f2ab86d6d2171753d5575e809cec9fca8
Erick Otenyo <otenyo.erick@gmail.com>
2024-07-23 21:51:05 +0300

    Add MEDIA_ROOT variable

--- wis2box_adl/src/wis2box_adl/config/settings/base.py
+++ wis2box_adl/src/wis2box_adl/config/settings/base.py
@@ -173,7 +173,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = env.str("MEDIA_ROOT", "/wis2box_adl/media")
 MEDIA_URL = "/media/"
 
 # Default storage settings, with the staticfiles storage updated.
