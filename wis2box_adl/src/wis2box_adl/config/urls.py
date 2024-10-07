00b4329ada5a8b78f1299508ac0551a3d7215706
Erick Otenyo <otenyo.erick@gmail.com>
2024-10-07 18:46:11 +0300

    Update base urls.py

--- wis2box_adl/src/wis2box_adl/config/urls.py
+++ wis2box_adl/src/wis2box_adl/config/urls.py
@@ -1,15 +1,13 @@
 from django.conf import settings
-from django.urls import include, path
 from django.contrib import admin
-
+from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path("django-admin/", admin.site.urls),
-    path("admin/", include(wagtailadmin_urls)),
-    path("documents/", include(wagtaildocs_urls)),
+    # path("django-admin/", admin.site.urls),
+    path("", include(wagtailadmin_urls)),
+    # path("documents/", include(wagtaildocs_urls)),
 ]
 
 if settings.DEBUG:
@@ -20,12 +18,12 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
-]
+# urlpatterns = urlpatterns + [
+#     # For anything not caught by a more specific rule above, hand over to
+#     # Wagtail's page serving mechanism. This should be the last pattern in
+#     # the list:
+#     path("", include(wagtail_urls)),
+#     # Alternatively, if you want Wagtail pages to be served from a subpath
+#     # of your site, rather than the site root:
+#     #    path("pages/", include(wagtail_urls)),
+# ]
