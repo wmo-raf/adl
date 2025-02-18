d5d8aa8cb7dc62f3c982610ce18df58f93e9be25
Erick Otenyo <otenyo.erick@gmail.com>
2025-02-18 15:22:46 +0000

    Add django admin url by default

--- adl/src/adl/config/urls.py
+++ adl/src/adl/config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("plugins/", include("adl.core.urls", namespace="plugins")),
     path("api/", include(api_urls)),
     path("", include(wagtailadmin_urls)),
+    path("debug/django-admin/", admin.site.urls)
     # path("documents/", include(wagtaildocs_urls)),
 ]
 
@@ -21,8 +22,6 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-    urlpatterns = [path("debug/django-admin/", admin.site.urls)] + urlpatterns
 
 # urlpatterns = urlpatterns + [
 #     # For anything not caught by a more specific rule above, hand over to
