0a20a8bd7c2352be2a0a015d0636db77732acda7
Erick Otenyo <otenyo.erick@gmail.com>
2025-08-15 11:14:21 +0300

    Update plugin default methods

--- plugin-boilerplate/{{ cookiecutter.project_slug }}/plugins/{{ cookiecutter.project_module }}/src/{{ cookiecutter.project_module }}/plugins.py
+++ plugin-boilerplate/{{ cookiecutter.project_slug }}/plugins/{{ cookiecutter.project_module }}/src/{{ cookiecutter.project_module }}/plugins.py
@@ -4,9 +4,9 @@ from adl.core.registries import Plugin
 class PluginNamePlugin(Plugin):
     type = "{{ cookiecutter.project_module }}"
     label = "{{ cookiecutter.project_name }}"
-
+    
     def get_urls(self):
         return []
-
-    def get_data(self):
+    
+    def get_station_data(self):
         return []
