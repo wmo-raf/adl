99ed15d017fa883fb4cc70e884919ec28e9cc952
Erick Otenyo <otenyo.erick@gmail.com>
2025-08-21 13:17:34 +0300

    Update StationRecordModel to allow string values

--- adl/src/adl/core/validators.py
+++ adl/src/adl/core/validators.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 
 from pydantic import BaseModel, Field
 
 
 class StationRecordModel(BaseModel):
     observation_time: datetime = Field(..., description="Timestamp of observation")
-    values: Dict[str, Optional[float]] = Field(default_factory=dict)
+    values: Dict[str, Optional[Union[str, int, float]]] = Field(default_factory=dict)
