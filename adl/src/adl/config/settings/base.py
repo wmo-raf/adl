2fa8a7fb6a8c172ad74fe0e0ed3c1c50707cd075
Oscar LASM <lasmo@mfi.fr>
2025-10-21 11:02:27 +0200

    proposes using a directory to centralize plugin sources

--- adl/src/adl/config/settings/base.py
+++ adl/src/adl/config/settings/base.py
@@ -100,14 +100,12 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-ADL_PLUGIN_DIRS = env.list("ADL_PLUGIN_DIRS", default=["/adl/plugins", ])
+ADL_PLUGIN_DIR = Path(env.str("ADL_PLUGIN_DIR", default="/adl/plugins"))
 
 ADL_PLUGIN_FOLDERS = []
-for plugin_dir in ADL_PLUGIN_DIRS:
-    plugin_dir = Path(plugin_dir)
-    if plugin_dir.exists():
-        plugin_folders = [file for file in plugin_dir.iterdir() if file.is_dir()]
-        ADL_PLUGIN_FOLDERS.extend(plugin_folders)
+if ADL_PLUGIN_DIR.exists():
+    plugin_folders = [file for file in ADL_PLUGIN_DIR.iterdir() if file.is_dir()]
+    ADL_PLUGIN_FOLDERS.extend(plugin_folders)
 
 ADL_PLUGIN_NAMES = [d.name for d in ADL_PLUGIN_FOLDERS]
 
