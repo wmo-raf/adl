ee1a1b0b385e3de390a02dc5e9f3c1936abf36a2
Erick Otenyo <otenyo.erick@gmail.com>
2026-04-13 18:53:54 +0300

    docs: simplify autodoc setup by installing real dependencies

--- docs/docs_settings.py
+++ docs/docs_settings.py
@@ -1,4 +1,18 @@
 SECRET_KEY = "docs-build-only"
+DEBUG = False
+USE_TZ = True
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+WAGTAIL_SITE_NAME = "ADL Docs"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
@@ -13,27 +27,37 @@ INSTALLED_APPS = [
     "wagtail.search",
     "wagtail.admin",
     "wagtail",
+    "modelcluster",
     "taggit",
+    "polymorphic",
+    "django_countries",
+    "wagtailgeowidget",
+    "wagtail_modeladmin",
+    "rest_framework",
+    "rest_framework_api_key",
+    "rest_framework_simplejwt",
+    "oauth2_provider",
     "django_celery_beat",
     "django_celery_results",
-
+    "django_eventstream",
+    "drf_spectacular",
+    "allauth",
+    "allauth.account",
+    "channels",
+    "wagtailiconchooser",
+    "wagtailfontawesomesvg",
+    "enum_intflagfield",
+    "adl.core",
+    "adl.monitoring",
 ]
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
-    }
-}
-USE_TZ = True
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-WAGTAIL_SITE_NAME = "ADL Docs"
-STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
+
 MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
+
 TEMPLATES = [{
     "BACKEND": "django.template.backends.django.DjangoTemplates",
     "DIRS": [],
@@ -46,3 +70,8 @@ TEMPLATES = [{
         ],
     },
 }]
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
